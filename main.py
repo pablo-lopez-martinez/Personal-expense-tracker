24cc5b80d8e4213d568f66ac9e6ad37367d1937d
pablo-lopez-martinez <paaaablo7@gmail.com>
2024-08-30 13:54:51 +0200

    Created gitignore for the finance_data document

--- main.py
+++ main.py
@@ -1,37 +1,18 @@
 import pandas as pd
 import matplotlib
 import csv 
-from user_interface import *
+from user_input import *
+from csv_handling import CSV
 import os
-
-class CSV:
-    CSV_FILE = "finance_data.csv"
-    COLUMS = ["date", "amount", "category", "description"]
-
-    @classmethod
-    def initialize_csv(cls):
-        try:
-            pd.read_csv(cls.CSV_FILE)
-        except FileNotFoundError:
-            df = pd.DataFrame(columns=cls.COLUMS)
-            df.to_csv(cls.CSV_FILE, index=False)
-
-    @classmethod 
-    def add_entry(cls, date, amount, category, description):
-        entry = {
-            "date": date,
-            "amount": amount,
-            "category": category,
-            "description": description
-        }
-        with open(cls.CSV_FILE, "a", newline="") as csvfile:
-            writer = csv.DictWriter(csvfile, fieldnames=cls.COLUMS)
-            writer.writerow(new_entry)
-        print("Entry added succesfully")
-
 
 def main():
     CSV.initialize_csv()
+    CSV.add_entry("12-02-2024", 3000, "Expense", "Supermarket")
+    CSV.add_entry("13-02-2024", 3000, "Expense", "Supermarket")
+    CSV.add_entry("14-02-2024", 3000, "Expense", "Supermarket")
+    CSV.add_entry("15-02-2024", 3000, "Expense", "Supermarket")
+    CSV.add_entry("16-02-2024", 3000, "Expense", "Supermarket")
+    print(CSV.get_entries("13-02-2024", "15-02-2024"))
 
 
 if __name__ == "__main__":
